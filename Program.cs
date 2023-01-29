d01b5cbe7c0fece3ad6fa4920145eb614503385a
olia115 <122738326+olia115@users.noreply.github.com>
2023-01-29 18:36:35 +0200

    add Program

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    //{//1.6
+    //double s = 0;
+    //    Console.Write("Введите число x=");
+    //    int x = Convert.ToInt32(Console.ReadLine());
+    //    if (x >= 0)
+
+    //    {
+    //        s = Math.Pow(x, 2) * 2 + 5;
+    //    }
+    //    else if (x < 0)
+    //    {
+    //        s = Math.Pow(x, 2) * -3;
+    //    }
+    //    Console.WriteLine(s);
+    //}
+    //вариант2
+    //{
+       //Console.Write("Введите число x=");
+       // double x = Convert.ToInt32(Console.ReadLine());
+       // double y;
+       // if (x <= -5)
+       // {
+       //     if (x == 0)  y = Math.Pow(x, 2); else y = Math.Pow(x, 2) + 15 * x;
+       // }
+       // if (x >= 5)
+       // {
+       //     y = (3 * x + 1) / 5;
+       // }
+       // else
+       // {
+       //     y = Math.Sqrt(13 + x);
+       // }
+       // Console.WriteLine(y);
+
+
+    //}
+}
