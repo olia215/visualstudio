ac09f0026e6f65a0f679e7819b85789dea7c30dc
olia115 <122738326+olia115@users.noreply.github.com>
2023-01-29 18:40:45 +0200

    add Prakt4tema6

---
+++ Prakt4tema6.cs
@@ -0,0 +1,169 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {//Задача1
+        //Console.WriteLine("Введите число от 1 до 7");
+        //int n = int.Parse(Console.ReadLine());
+        //switch (n)
+        //{
+        //    case 1:
+        //        Console.WriteLine("Понедельник");
+        //        break;
+        //    case 2:
+        //        Console.WriteLine("Вторик");
+        //        break;
+        //    case 3:
+        //        Console.WriteLine("Среда");
+        //        break;
+        //    case 4:
+        //        Console.WriteLine("Четверг");
+        //        break;
+        //    case 5:
+        //        Console.WriteLine("Пятница");
+        //        break;
+        //    case 6:
+        //        Console.WriteLine("Суббота");
+        //        break;
+        //    case 7:
+        //        Console.WriteLine("Воскресенье");
+        //        break;
+        //}
+
+        //Задача2
+        //Console.WriteLine("Введите номер месяца от 1 до 12");
+        //int n = int.Parse(Console.ReadLine());
+        //switch (n)
+        //{
+        //    case 1:
+        //        Console.WriteLine("Январь");
+        //        break;
+        //    case 2:
+        //        Console.WriteLine("Февраль");
+        //        break;
+        //    case 3:
+        //        Console.WriteLine("март");
+        //        break;
+        //    case 4:
+        //        Console.WriteLine("Апрель");
+        //        break;
+        //    case 5:
+        //        Console.WriteLine("Май");
+        //        break;
+        //    case 6:
+        //        Console.WriteLine("Июнь");
+        //        break;
+        //    case 7:
+        //        Console.WriteLine("Июль");
+        //        break;
+        //    case 8:
+        //        Console.WriteLine("Август");
+        //        break;
+        //    case 9:
+        //        Console.WriteLine("Сентябрь");
+        //        break;
+        //    case 10:
+        //        Console.WriteLine("Октябрь");
+        //        break;
+        //    case 11:
+        //        Console.WriteLine("Ноябрь");
+        //        break;
+        //    case 12:
+        //        Console.WriteLine("Декабрь");
+        //        break;
+
+
+        //}
+        //Задача3
+        //Console.Write("Введите один из признаков: a – автомобиль, в – велосипед, м - мотоцикл, с – самолет, п – поезд");
+        //string n = Console.ReadLine();
+        //switch (n)
+        //{
+        //    case "а":
+        //        Console.WriteLine("500км/ч");
+        //        break;
+        //    case "в":
+        //        Console.WriteLine("300км/ч");
+        //        break;
+        //    case "м":
+        //        Console.WriteLine("200км/ч");
+        //        break;
+        //    case "с":
+        //        Console.WriteLine("360км/ч");
+        //        break;
+        //    case "п":
+        //        Console.WriteLine("600км/ч");
+        //        break;
+
+
+        //}
+        //Задача4
+        //Console.WriteLine("Введите один из признаков: ф – физика, м – математика, и – история, г – география, б – биология");
+        //string n = Console.ReadLine();
+        //switch (n)
+        //{
+        //    case "ф":
+        //        Console.WriteLine("физика");
+        //        break;
+        //    case "м":
+        //        Console.WriteLine("математика");
+        //        break;
+        //    case "и":
+        //        Console.WriteLine("история");
+        //        break;
+        //    case "г":
+        //        Console.WriteLine("география");
+        //        break;
+        //    case "б":
+        //        Console.WriteLine("биология");
+        //        break;
+
+
+        //}
+        //Задача5
+        // Console.WriteLine("Введите название месяца ");
+        //string n = Console.ReadLine();
+        //switch (n)
+        //{
+        //    case "Январь" :
+        //        Console.WriteLine("31");
+        //        break;
+        //    case "Февраль":
+        //        Console.WriteLine("28");
+        //        break;
+        //    case "Март":
+        //        Console.WriteLine("31");
+        //        break;
+        //    case "Апрель":
+        //        Console.WriteLine("30");
+        //        break;
+        //    case "Май":
+        //        Console.WriteLine("31");
+        //        break;
+        //    case "Июнь":
+        //        Console.WriteLine("30");
+        //        break;
+        //    case "Июль":
+        //        Console.WriteLine("31");
+        //        break;
+        //    case "Август":
+        //        Console.WriteLine("31");
+        //        break;
+        //    case "Сентябрь":
+        //        Console.WriteLine("30");
+        //        break;
+        //    case "Октябрь":
+        //        Console.WriteLine("31");
+        //        break;
+        //    case "Ноябрь":
+        //        Console.WriteLine("30");
+        //        break;
+        //    case "Декабрь":
+        //        Console.WriteLine("31");
+        //        break;
+
+
+        //}
+
+
+    }
+}
