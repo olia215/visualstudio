47fa8c9a32263c75b1b8a2188ffc62fdcc500db7
olia115 <122738326+olia115@users.noreply.github.com>
2023-01-29 18:43:58 +0200

    add tema6prakt6

---
+++ tema6prakt6.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Задание1 1)
+         //Console.WriteLine("Введите число");
+         //int a = Convert.ToInt32(Console.ReadLine());
+         //if (a % 3 == 0)
+         //{
+         //    Console.WriteLine("Число является делителем числа 3");
+         //}
+         //else
+         //{
+         //    Console.WriteLine("Число не является делителем");
+         //}
+         //Console.ReadLine();
+         //2)
+         //Console.WriteLine("Введите число");
+         //int a = Convert.ToInt32(Console.ReadLine());
+         //if (a % 4 == 0)
+         //{
+         //    Console.WriteLine("Число является кратным 4");
+         //}
+         //else
+         //{
+         //    Console.WriteLine("Число не является кратным 4");
+         //}
+         //Console.ReadLine();
+         //3)
+         //Console.WriteLine("Введите число a");
+         //double a = double.Parse(Console.ReadLine());
+         //Console.WriteLine("Введите число b");
+         //double b = double.Parse(Console.ReadLine());
+         //if (b >= a)
+         //{
+         //    Console.WriteLine("Число a может являтся делителем числа b");
+         //}
+         //else
+         //{
+         //    Console.WriteLine("Число a не может являться делителем числа b");
+         //}
+         //Console.ReadLine();
+         //4)
+         //Console.WriteLine("Введите число a");
+         //double a = double.Parse(Console.ReadLine());
+         //Console.WriteLine("Введите число b");
+         //double b = double.Parse(Console.ReadLine());
+         //Console.WriteLine(Convert.ToString((a / b) - Math.Truncate(a/b)));
+
+            //Console.ReadLine();
+            //Задание 2
+            //1)
+            //Console.WriteLine("Введите первое число");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите второе число");
+            //double b = double.Parse(Console.ReadLine());
+            //if (a < b)
+            //{
+            //    Console.WriteLine($"{a}+{b}={a + b}");
+            //}
+            //else if (a > b)
+            //{
+            //    Console.WriteLine($"{a}*{b}={a * b}");
+            //}
+            //if (b > a)
+            //{
+            //    Console.WriteLine($"{a}*{b}={a * b}");
+            //}
+            //else if (a > b)
+            //{
+            //    Console.WriteLine($"{a}+{b}={a + b}");
+            //}
+            //2)
+            //Console.WriteLine("Введите первое число");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите второе число");
+            //double b = double.Parse(Console.ReadLine());
+            //if (a < b)
+            //{
+            //    Console.WriteLine($"({a}+{b})/2={(a + b)/2}");
+            //}
+            //else if (a > b)
+            //{
+            //    Console.WriteLine($"({a}*{b})*2={(a * b)*2}");
+            //}
+            //if (b > a)
+            //{
+            //    Console.WriteLine($"({a}*{b})*2={(a * b)*2}");
+            //}
+            //else if (a > b)
+            //{
+            //    Console.WriteLine($"({a}+{b})/2={(a + b)/2}");
+            //}
+
+
+
+
+
+
+        }
+    }
+}
