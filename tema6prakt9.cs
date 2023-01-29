fa0a6903f3ae468dc4e8e706257d22831b2ae7d7
olia115 <122738326+olia115@users.noreply.github.com>
2023-01-29 18:47:11 +0200

    add tema6prakt9

---
+++ tema6prakt9.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { //1
+          //for (int x = 10; x <= 99; x++)
+          //{
+          //    if (x % 2 == 0)
+          //    {
+          //        Console.WriteLine(x);
+          //    }
+          //}
+          //2
+          //int Summ = 0;
+          //for (int x = 10; x <= 99; x++)
+          //{
+          //    if (x % 25 == 0)
+          //    {
+          //        Summ = Summ + x;
+          //    }
+          //}
+          //    Console.Write($"Summ={Summ}");
+          //Console.ReadKey();
+          //3
+
+            //for (int i = 1; i <= 10; i++)
+
+            //{
+            //    Console.WriteLine(i + " * 3 = " + i*3);
+            //}
+            //4
+            //for (int i = 1; i <= 20;)
+            //{
+            //    Console.WriteLine("y=" + (5 * i - 2));
+            //    i += 2;
+            //}
+            //5
+            //for (int i = 1; i < 12; i++)
+            //{ if (i!=3)
+            //    Console.Write(i + ", ");
+            //    i += 1;
+            //}
+                
+
+        }
+    }
+}
