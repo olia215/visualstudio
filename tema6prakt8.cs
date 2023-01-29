6907ae60ec2bf71fd59513ce985a3194f224ecaa
olia115 <122738326+olia115@users.noreply.github.com>
2023-01-29 18:46:16 +0200

    add tema6prakt8

---
+++ tema6prakt8.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//1 Задание 1)
+         //for (int i = 1; i < 11; i++)
+         //{
+         //    Console.Write("#");
+         //}
+         //for (int j = 1; j < 13; j++)
+         //{
+         //    Console.WriteLine("#");
+         //}
+         //2)
+         //for (int i = 1; i < 11; i++)
+         //{
+
+            //    Console.Write("#");
+            //}
+            //Console.WriteLine("");
+            //for (int j = 1; j < 13; j++)
+            //{ for (int k = 1; k < 10; k++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    Console.WriteLine("#");
+            //}
+            //3)
+            //for (int i = 1; i < 16; i++)
+            //{
+            //    Console.WriteLine("#");
+            //}
+            //for (int j = 1; j < 13; j++)
+            //{
+            //    Console.Write("#");
+            //}
+            //4)
+            //for (int i = 1; i < 13; i++)
+            //{
+            //    for (int k = 1; k < 10; k++)
+            //    {
+            //        Console.Write(" ");
+
+            //    }
+            //    Console.WriteLine("#");
+            //}
+            //for (int j = 1; j < 11; j++)
+            //{
+            //    Console.Write("#");
+            //}
+            //Задание 2
+            //for (int i = 1; i < 17; i++)
+            //{
+            //    Console.Write("#");
+            //}
+            //Console.WriteLine("");
+            //for (int j = 1; j < 18; j++)
+            //{
+            //    Console.Write("#");
+            //    for (int k = 1; k < 15; k++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    Console.WriteLine("#");
+
+            //}
+            //for (int l = 1; l < 17; l++)
+            //{
+            //    Console.Write("#");
+            //}
+            //Задание 3
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    for (int j = 0; j < 5 - i; j++)
+            //    {
+            //        Console.Write(j);
+            //    }
+            //    Console.WriteLine("");
+            //}
+            //Задание 4
+            //for (int j = 1; j < 5; j++)
+            //{
+
+            //    for (int i = 1; i < 6; i++)
+            //    {
+            //        Console.Write("2");
+            //    }
+            //    Console.WriteLine("");
+            //} 
+
+        }
+    }
+}
