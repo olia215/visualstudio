9c19848bd27b8755c2b6e09f0a1cedc948af21c2
olia115 <122738326+olia115@users.noreply.github.com>
2023-01-29 18:42:36 +0200

    add tema6prakt5

---
+++ tema6prakt5.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            //Console.Write("Программирование на С#!");
+            //Console.ReadLine();
+            //2
+            //int num = 20;
+            //num += 5;
+            //Console.WriteLine("Переменная:var= "+num);
+            //3
+            //double a=1.4234;
+            //a = Math.Pow(a, 3);
+            //a = Math.Round(a, 0);
+            //Console.Write("a="+a);
+            //4
+            //double a;
+            //Console.Write("Введите число=");
+            //a=double.Parse(Console.ReadLine());
+            //if (a > 5 & a < 10)
+            //{
+            //    Console.Write("Число больше 5 и меньше 10");
+            //}
+            //else
+            //{
+            //    Console.Write("Неизвестное число");
+            //}
+            //5
+            //double a;
+            //double b;
+            //Console.Write("Ввведите первое число a=");
+            //a = double.Parse(Console.ReadLine());
+            //Console.Write("Введите второе число b=");
+            //b = double.Parse(Console.ReadLine());
+            //if (a == b | a > b | a < b)
+            //{                                   
+            //    if (a == b) Console.WriteLine("Два числа равны "+a + "=" + b); else if (a > b) Console.WriteLine("Первое число больше второго "+a + ">" + b); else if (a < b) Console.WriteLine("Первое число меньше второго "+a + "<" + b);
+            //}
+            //Console.ReadLine();
+            //6
+            //Console.WriteLine("Введите номер месяца от 1 до 12");
+            //int n = int.Parse(Console.ReadLine());
+            //switch (n)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Зима");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Зима");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Весна");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Весна");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Весна");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("Лето");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("Лето");
+            //        break;
+            //    case 8:
+            //        Console.WriteLine("Лето");
+            //        break;
+            //    case 9:
+            //        Console.WriteLine("Осень");
+            //        break;
+            //    case 10:
+            //        Console.WriteLine("Осень");
+            //        break;
+            //    case 11:
+            //        Console.WriteLine("Осень");
+            //        break;
+            //    case 12:
+            //        Console.WriteLine("Зима");
+            //        break;
+
+            //}
+            //7
+            //Console.Write("Выбирете фигуру:  к – круг, п – прямоугольник, т - треугольник ");
+            //string a;
+            //a = Console.ReadLine();
+            //double b, s, p, l1, l2, c1, c2, c3;
+            //switch (a)
+            //{
+            //    case "к":
+            //        Console.WriteLine("Укажите длинну радиуса=");
+            //        b = double.Parse(Console.ReadLine());
+            //        s = Math.Round(3.14 * Math.Pow(b, 2));
+            //        p = Math.Round(2 * 3.14 * b);
+            //        Console.WriteLine("p=" + p);
+            //        Console.WriteLine("s=" + s);
+            //        break;
+            //    case "п":
+            //        Console.WriteLine("Укажите длинну и ширину=");
+            //        l1 = double.Parse(Console.ReadLine());
+            //        l2 = double.Parse(Console.ReadLine());
+            //        s = l1 * l2;
+            //        p = (l1 + l2) * 2;
+            //        Console.WriteLine("s=" + s);
+            //        Console.WriteLine("p=" + p);
+            //        break;
+            //    case "т":
+            //        Console.WriteLine("Укажите 3 стороны и высоту =");
+            //        c1 = double.Parse(Console.ReadLine());
+            //        c2 = double.Parse(Console.ReadLine());
+            //        c3 = double.Parse(Console.ReadLine());
+            //        p = c1 + c2 + c3;
+            //        s = p / 2;
+            //        s = Math.Sqrt(s * (s - c1) * (s - c2) * (s - c3));
+            //        Console.WriteLine("s=" + s);
+            //        Console.WriteLine("p=" + p);
+            //        break;
+            //}
+
+
+
+
+        }
+    }
+}
