47e5d438c225d13eb73333c3c266602c62cca0ad
olia115 <122738326+olia115@users.noreply.github.com>
2023-01-29 18:45:17 +0200

    add tema6prakt7

---
+++ tema6prakt7.cs
@@ -0,0 +1,58 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {//1
+     //for (int i = 1; i < 19; i++)
+     //{
+     //    Console.Write("#");
+
+
+        //}
+        //2
+        //for (int i = 1; i < 26; i++)
+        //{
+        //    Console.WriteLine("#");
+        //}
+        //3
+        //for (int i = 1; i < 5; i++)
+        //{
+        //    Console.WriteLine();
+        //}
+        //for (int i = 1; i < 21; i++)
+        //{
+        //    Console.Write("#");
+        //}
+        //4
+        //for (int i = 1; i < 19; i++)
+        //{
+        //    for (int j = 1; j < 6; j++)
+        //    {
+        //        Console.Write("");
+        //    }
+        //    Console.WriteLine("#");
+        //}
+        //5
+        //for (int i = 1; i < 6; i++)
+        //{
+        //    for (int j = 1; j < 16; j++)
+        //    {
+        //        Console.Write("#");
+        //    }
+        //    Console.WriteLine();
+        //}
+        //6
+        //int j = 0;
+        //do
+        //{
+        //    for (int i = 1; i < 8; i++)
+        //    {
+        //        Console.Write("#");
+        //    }
+        //    Console.WriteLine(); Console.WriteLine();
+        //    j++;
+        //}
+        //while (j < 4);
+
+            
+    }
+}
