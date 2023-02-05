a0e458743c795f463d931c6ba58cd2951647b099
olia115 <122738326+olia115@users.noreply.github.com>
2023-02-05 18:57:29 +0200

    add DomRabye

---
+++ DomRabye lf.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            //int[] omas = new int[9];
+            //for (int i = 0; i < 9; i++)
+            //{
+            //    Console.Write($"omas [{i}]=");
+            //    omas[i] = int.Parse(Console.ReadLine());
+            //}
+            //foreach (int elem in omas)
+            //{
+            //    Console.Write($"{elem}\t");
+            //}
+            //2
+            //int n = int.Parse(Console.ReadLine());
+            //double[] omas = new double [n];
+            //for ( int i=0; i<n; i++)
+            //{
+            //    Console.Write($"omas [{i}]=");
+            //    omas[i] = double.Parse(Console.ReadLine());
+
+            //}
+            //foreach (double elem in omas)
+            //{
+            //    Console.Write($"{Math.Abs(elem)}\t");
+            //}
+            //3
+            //int[] omas = new int[6] { -7, 10, 5, 16, -4, 18 };
+            //for (int i = 5; i >= 0; i--)
+            //{
+            //    Console.WriteLine($"omas [{i}]=" + omas[i]);
+            //}
+            //4
+            //int[] omas = new int[12];
+            //Random x = new Random();
+            //foreach (int elem in omas)
+            //{
+            //    Console.WriteLine($"{x.Next(163, 191)}");
+            //}
+
+        }
+    }
+}
