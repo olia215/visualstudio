28c2c3a5546b169e16e760ed830f47810122fcc4
olia115 <122738326+olia115@users.noreply.github.com>
2023-02-12 16:30:17 +0200

    add tema9

---
+++ tema9.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {//1
+     //int[] mas = new int[10];
+     //Random rnd = new Random();
+     //int i;
+     //for (i = 0; i <= 9; i++)
+     //{
+     //    mas[i] = rnd.Next(-10, 10);
+     //    Console.Write($"mas[{i}]={mas[i]}\n");
+     //}
+     //Console.WriteLine();
+     //int index = 0;
+     //for (i = 0; i <= 9; i++)
+     //{
+     //    if (mas[i] % 2==0)
+     //    {
+
+
+        //        index++;
+        //    }
+        //}
+        //Console.WriteLine($"i={index}");
+        //if (index == 0)
+        //{
+        //    Console.WriteLine("Чисел четных нет");
+        //}
+        //Console.ReadLine();
+        //2
+        //Console.Write("Введите размерность массива:");
+        //int N= int.Parse(Console.ReadLine());
+        //double[] mas = new double[N];
+        //Random rnd = new Random();
+        //int i;
+        //for (i = 0; i <= N-1; i++)
+        //{
+        //    mas[i] = rnd.NextDouble() + rnd.Next(-100,100) ;
+        //    Console.Write($"mas[{i}]={mas[i]}\n");
+        //}
+        //Console.WriteLine();
+        //double index = 0;
+        //for (i = 0; i <= N-1; i++)
+        //{
+        //    if (mas[i] % 5==0)
+        //    {
+        //        index = mas[i] + index;
+        //    }
+        //}
+        //Console.WriteLine($"i={index}");
+        //if (index == 0)
+        //{
+        //    Console.WriteLine("Чисел кратных 5 нет ");
+        //}
+        //Console.ReadLine();
+        //3
+        //int[] myArray = { 4, 7, 11, 2 };
+        //float sr_ar = 0;
+        //int l = 0;
+        //for (int i = 1; i <= 4; i++)
+        //    if (i % 2 == 0)
+        //    {
+        //        sr_ar += myArray[i-1];
+        //        l++;
+        //    }
+        //        Console.WriteLine("Среднее арифметическое ={0}", sr_ar/l);
+        //Console.ReadKey();
+        //4
+        //int[] omas = new int[6];
+
+        //for (int i = 0; i <= 5; i++)
+        //{
+        //    Console.Write($"omas[{i}]=");
+        //    omas[i] = int.Parse(Console.ReadLine());
+        //}
+
+        //for (int i = 0; i <= 5; i++)
+        //{
+        //    omas[i] *= 2;
+        //}
+
+        //for (int i = 0; i < 6; i++)
+        //{
+        //    Console.WriteLine($"omas[{i}]= {omas[i]}");
+        //}
+        //Console.ReadKey();
+        //5
+        //Console.Write("Введите размерность массива:");
+        //int N = int.Parse(Console.ReadLine());
+        //int[] mas = new int[N];
+        //Random rnd = new Random();
+        //int i;
+        //for (i = 0; i <= N - 1; i++)
+        //{
+        //    mas[i] =  rnd.Next(-100, 100);
+        //    Console.Write($"mas[{i}]={mas[i]}\n");
+        //}
+        //Console.WriteLine();
+        //int index = 0;
+        //for (i = 0; i <= N - 1; i++)
+        //{
+        //    if (mas[i] < 0)
+        //    {
+        //        index++;
+        //    }
+        //}
+
+        //int[] mas1 = new int[index];
+        //int i1 = 0;
+        //for (i = 0; i <= N - 1; i++)
+        //{ 
+        //    if (mas[i] < 0)
+        //    {
+        //        mas1[i1] = mas[i];
+        //        i1++;
+        //    }
+        //}
+
+        //int[] mas2 = new int[N-index];
+        //int i2 = 0;
+        //for (i = 0; i <= N - 1; i++)
+        //{
+        //    if (mas[i] >= 0)
+        //    {
+        //        mas2[i2] = mas[i];
+        //        i2++;
+        //    }
+        //}
+        //foreach (int elem in mas1)
+        //{
+        //    Console.Write($"{elem}\t");
+        //}
+        //Console.WriteLine();
+        //foreach (int elem in mas2)
+        //{
+        //    Console.Write($"{elem}\t");
+        //}
+
+
+        //Console.ReadLine();
+        ////6
+        //Console.Write("Введите размерность массива:");
+        //int N = int.Parse(Console.ReadLine());
+        //int[] mas = new int[N];
+        //int i;
+        //for (i = 0; i <= N - 1; i++)
+        //{
+        //    mas[i] = int.Parse(Console.ReadLine());
+        //}
+        //int max = mas[0]; int index1 = 1;
+        //for (i = 1; i <= N; i++)
+        //{
+        //    if (mas[i-1] > max)
+        //    {
+        //        max = mas[i-1];
+        //        index1 = i;
+        //    }
+        //}
+
+        //int min = mas[0]; int index2 = 1;
+        
+        //for (i = 1; i <= N; i++)
+        //{
+        //    if (mas[i-1] < min)
+        //    {
+        //        min = mas[i-1];
+        //        index2 = i;
+        //    }
+        //}
+
+        //double omas;
+        //omas= (index1 + index2) / 2;
+        //Console.Write($"Среднее арифметическое максимального и минимального индексов: {omas}"); 
+
+       
+
+
+    }
+
+
+}
+
+
