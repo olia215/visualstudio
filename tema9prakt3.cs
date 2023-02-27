5622437906258d624f28f8c5c6944df0b5f65482
olia115 <122738326+olia115@users.noreply.github.com>
2023-02-27 09:09:00 +0200

    add tema9prakt3

---
+++ tema9prakt3.cs
@@ -0,0 +1,74 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Задание 1
+            // Console.Write("n=");
+            // int n=Convert.ToInt32(Console.ReadLine());
+            // int[] mass = new int[n];
+            // for(int i=0;i<n;i++)
+            // {
+            //     Console.Write($"mass[{i}]=");
+            //     mass[i]=Convert.ToInt32(Console.ReadLine());
+            // }
+            // for(int i=0;i<n;i++)
+            // {
+            //     Console.WriteLine($"mass[{i}]={mass[i]}");
+            // }
+
+        // Задание 2
+        // Console.Write("n=");
+        // int n=Convert.ToInt32(Console.ReadLine());
+        // int[] mass = new int[n];
+        // Random random = new Random();
+        // for(int i=0;i<n;i++)
+        // {
+        //     масса[i] = random.Next(-10,10);
+        // }
+        // foreach(целый элемент в массиве)
+        // {
+        //     Console.WriteLine($"{elem}");
+        // }
+
+        // Задача 3
+        // Console.Write("n=");
+        // int n=Convert.ToInt32(Console.ReadLine());
+        // int[] mass = new int[n];
+        // for(int i=0;i<n;i++)
+        // {
+        //     Console.Write($"mass[{i}]=");
+        //     масса[i] = Convert.ToInt32(Console.ReadLine());
+        // }
+        // int max = масса[0];
+        // int index1 = 0;
+        // интервал мин = масса[0];
+        // int index2 = 0;
+        // for(int i=0;i<n;i++)
+        // {
+        //     если (масса[i]>max)
+        //     {
+        //         max= масса[i];
+        //         индекс1 = я;
+        //     }
+        //     если(масса[i]<мин)
+        //     {
+        //         мин= масса[i];
+        //         индекс2 = я;
+        //     }
+        // }
+        // Console.WriteLine($"max={max}");
+        // Console.WriteLine($"index1={index1}");
+        // Console.WriteLine($"min={min}");
+        // Console.WriteLine($"index2={index2}");
+        // int new_min = max;
+        // int new_max = мин;
+        // for(int i=0;i<n;i++)
+        // {
+        //     Console.WriteLine($"mass[{i}]={mass[i]}");
+        // }
+        // Console.WriteLine($"new_max={new_max}");
+        // Console.WriteLine($"new_min={new_min}");
+        // Console.WriteLine($"Количество элементов между максимальным и минимальным значением={Math.Abs(index2-index1)-1}");
+            
+    }
+}
