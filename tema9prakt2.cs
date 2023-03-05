1832d92c7da7af3fc75d420c4734da416a5994c1
olia115 <122738326+olia115@users.noreply.github.com>
2023-03-05 22:21:16 +0200

    add tema9prakt2

---
+++ tema9prakt2.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//1
+         //int[] mas = new int[6];
+         //Random rnd = new Random();
+         //int i;
+         //for (i = 0; i <= 5; i++)
+         //{
+         //    mas[i] = rnd.Next(0,6) ;
+         //    Console.Write($"mas[{i}]={mas[i]}\n");
+         //}
+         //Console.WriteLine();
+         //for (int l = 1; l < 5; l++)
+         //{
+         //    if (mas[0] == mas[l])
+         //    {
+         //        Console.Write(l);
+         //        break;
+
+            //    }
+
+            //}
+            //2 a)
+            //int[] mas = new int[20];
+            //Random rnd = new Random();
+
+            //for (int i = 0; i <= 19; i++)
+            //{
+            //    mas[i] = rnd.Next(-150,150);
+            //      Console.Write($"mas[{i}]={mas[i]}\n");
+            //}
+            //Console.WriteLine();
+            //for (int l = 0; l < 19; l++)
+            //{
+            //    if (mas[l] > 0)
+            //    {
+            //        Console.WriteLine(mas[l]);
+            //    }
+            //}
+            //б)
+            //int[] mas = new int[20];
+            //Random rnd = new Random();
+            //for (int i = 0; i <= 19; i++)
+            //{
+            //    mas[i] = rnd.Next(-150,150);
+            //     Console.Write($"mas[{i}]={mas[i]}\n");
+            //}
+            //Console.WriteLine();
+            //for (int l = 0; l < 19; l++)
+            //{
+            //    if (mas[l] <= 100)
+            //    {
+            //        Console.WriteLine(mas[l]);
+            //    }
+            //}
+            //в)
+            //int[] mas = new int[20];
+            //Random rnd = new Random();
+            //for (int i = 0; i <= 19; i++)
+            //{
+            //    mas[i] = rnd.Next(-150, 150);
+            //    Console.Write($"mas[{i}]={mas[i]}\n");
+            //}
+            //Console.WriteLine();
+            //for (int l = 0; l < 19; l++)
+            //{
+            //    if (mas[l] % 2==0)
+            //    {
+            //        Console.WriteLine(mas[l]);
+            //    }
+            //}
+            //3
+            //int[] mas = new int[12] { 5, -3, 4, -8, -9, 1, 9, 6, -3, 4, 8, 2 };
+            //int j=0;
+            //for (int i = 0; i < 12; i++)
+            //{
+            //    if (mas[i] < 0)
+            //    {
+            //        j++;
+            //    }
+            //}
+            //Console.Write(j);
+            //4
+            //int[] mas = new int[5];
+            //Random rnd = new Random();
+            //for (int i = 0; i <= 4; i++)
+            //{
+            //    mas[i] = rnd.Next(0, 10);
+            //    Console.Write($"mas[{i}]={mas[i]}\n");
+            //}
+            //Console.WriteLine();
+            //int o = 0;
+            //int y = 0;
+            //for (int l = 0; l < 4; l++)
+            //{
+            //    if (l % 2 != 0)
+            //    {
+
+            //        o = o + mas[l];
+            //        y++;
+            //    }
+            //}
+            //Console.Write(o/y);
+            //5
+            //int[] mas = new int[5];
+            //Random rnd = new Random();
+            //for (int i = 0; i <= 4; i++)
+            //{
+            //    mas[i] = rnd.Next(0, 10);
+            //    Console.Write($"mas[{i}]={mas[i]}\n");
+            //}
+            //for (int i = 1; i <= 4; i++)
+            //{
+            //    mas[i] += mas[0];
+            //}
+            //foreach (int elem in mas)
+            //{
+            //    Console.WriteLine(elem);
+            //}
+
+        }
+    }
+}
