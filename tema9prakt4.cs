71378f44ef47fc013c3132b4597c2c910435f5e1
olia115 <122738326+olia115@users.noreply.github.com>
2023-04-02 13:48:39 +0300

    add tema9prakt4

---
+++ tema9prakt4.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//1)
+         //int j = 0;
+
+            //int[] mass = new int[15] ;
+            //Random random = new Random();
+            //Console.WriteLine("Выводим элементы массива:");
+            //for (int  i=0; i<15; i++)
+            //{
+            //    mass[i] = random.Next(-50, 50);
+            //    Console.WriteLine(mass[i]);
+            //}
+            //Console.WriteLine("Выводим элементы массива кратные 3:");
+            //for (int i = 0; i < 15; i++)
+            //{
+
+            //    if (mass[i] % 3 == 0)
+            //    {
+            //        Console.WriteLine(mass[i]);
+
+            //        j++;
+            //    }
+            //}
+            //Console.WriteLine("количество массивов кратных 3:");
+            //Console.WriteLine(j);
+            //2)
+            //int min = 0;
+            //int maks = 0;
+            //int o = 0;
+            //int[] mass = new int[10];
+            //Random random = new Random();
+            //Console.WriteLine("Выводим элементы массива:");
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    mass[i] = random.Next(0, 100);
+            //    Console.WriteLine(mass[i]);
+            //}
+
+            //Console.WriteLine("наименьший элемент:");
+            //for(int j=1; j<10; j++ )
+            //{
+            //    if(mass[j]<mass[min])
+
+            //    {
+            //        min = j;  
+            //    }
+
+            //}
+            //Console.WriteLine(min);
+            //Console.WriteLine("Наибольший элемент:");
+            //for (int j = 1; j < 10; j++)
+            //{
+            //    if (mass[j]>mass[maks])
+            //    {
+            //       maks=j;
+            //    }
+
+            //}
+            //Console.WriteLine(maks);
+            //Console.WriteLine("Меняем местами:");
+            //o = mass[min];
+            //mass[min] = mass[maks];
+            //mass[maks] = o;
+            //for (int i = 0; i < 10; i++)
+            //{
+                
+            //    Console.WriteLine(mass[i]);
+            //}
+            
+
+
+        }
+    }
+}
