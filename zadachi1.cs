2d297dc41928e6a766614d3d03eecbb12abdbbe9
Оля 215 <122738326+olia215@users.noreply.github.com>
2023-10-14 23:21:50 +0300

    Add files via upload

---
+++ zadachi1.cs
@@ -0,0 +1,92 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Serialization.Formatters;
+using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {/* Задание 1*/
+        //Console.Write("Введите n :");
+        //int n = Convert.ToInt32(Console.ReadLine());
+        //for (int i =1; 1<=n; i++)
+        //{
+        //    Console.WriteLine(i);
+
+        //}
+        //Console.ReadKey();
+
+        //Console.Write("Введите n :");
+        //int n = Convert.ToInt32(Console.ReadLine());
+        //int  i = 0;
+        //do
+        //{
+        //    i++;
+        //    Console.WriteLine("" + i);
+        //}
+        //while (i < n);
+
+        //Console.Write("Введите n :");
+        //int n = Convert.ToInt32(Console.ReadLine());
+        //int i = 1;
+        //while (i<=n)
+        //{
+
+        //    Console.WriteLine(""+i);
+        //    i++;
+        //}
+        //Задание 2
+        //int a = 0;
+        //for (int i = 10; 1 <= 99; i++)
+        //{
+        //    if (i % 2 == 0)
+        //    {
+        //        a++;
+        //        Console.WriteLine(i);
+        //    }
+        //}
+        //Console.WriteLine($"Количество четных двухзначных чисел {a}");
+
+        //Задание 3
+        //for (double x = -2; x <= 2; x += 0.2)
+        //{
+        //    Console.WriteLine($"x={x}\t y-{3 * Math.Pow(x, 2) + 2 * x - 1}");
+        //}
+        //Задание 4
+        //int a = 0;
+        //for (int i = 10; 1 <= 99; i++)
+        //{
+        //    if (i % 5 == 0)
+        //    {
+        //        Sum = Sum + x;
+        //    }
+        //}
+        //Console.WriteLine(sum);
+
+        //Задание 5
+        //int a = 0;
+        //Console.Write("введите число:");
+        //int n = Convert.ToInt32(Console.ReadLine());
+        //foreach (int n in  "n")
+        //{
+        //    a++;
+        //}
+        //Console.WriteLine(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
