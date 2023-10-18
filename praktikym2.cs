2f401a8b829182c68323d2d50fce7b4470f9dedb
Оля 215 <122738326+olia215@users.noreply.github.com>
2023-10-18 23:45:47 +0300

    Add files via upload

---
+++ praktikym2.cs
@@ -0,0 +1,94 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {/* Задание 1*/
+        //Console.WriteLine("Введите название футбольного клуба:");
+        //string clubName = Console.ReadLine();
+        //int str = clubName.Length;
+        //Console.WriteLine("Количество символов в названии футбольного клуба: " + str);
+        //Задание 2
+        //Console.WriteLine("Введите название города:");
+        //string cityName = Console.ReadLine();
+        //int str = cityName.Length;
+        //if (str % 2 == 0)
+        //{
+        //    Console.WriteLine("Количество символов в названии города '" + cityName + "' четное.");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Количество символов в названии города '" + cityName + "' нечетное.");
+        //}
+        //Задание 3
+        //Console.WriteLine("Введите первую фамилию:");
+        //string str1 = Console.ReadLine();
+        //Console.WriteLine("Введите вторую фамилию:");
+        //string str2 = Console.ReadLine();
+        //int a = str1.Length;
+        //int b = str2.Length;
+        //if (a > b)
+        //{
+        //    Console.WriteLine("Первая фамилия (" + a + ") длиннее второй фамилии.");
+        //}
+        //else if (a < b)
+        //{
+        //    Console.WriteLine("Вторая фамилия (" + b + ") длиннее первой фамилии.");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Обе фамилии имеют одинаковую длину.");
+        //}
+        //Задание 4
+        //string str = "";
+        //Console.WriteLine("Введите пять слов:");
+        //for (int i = 0; i < 5; i++)
+        //{
+        //    Console.WriteLine("Слово " + (i + 1) + ":");
+        //    string x = Console.ReadLine();
+
+        //    if (x.Length > str.Length)
+        //    {
+        //        str = x;
+        //    }
+        //}
+        //Console.WriteLine("Самое длинное слово: " + str);
+        //Задание 5
+        //Console.WriteLine("Введите ваше имя:");
+        //string str1 = Console.ReadLine();
+        //Console.WriteLine("Введите вашу фамилию:");
+        //string str2 = Console.ReadLine();
+        //string str = str1 + " " + str2;
+        //Console.WriteLine("Ваше полное имя: " + str);
+        //Задание 6
+        //Console.WriteLine("Введите строку:");
+        //string st = Console.ReadLine();
+        //int count = 0;
+        //foreach (char c in st)
+        //{
+        //    if (c == 'а' || c == 'А')
+        //    {
+        //        count++;
+        //    }
+        //}
+        //Console.WriteLine("Количество символов 'а'('А') в строке: " + count);
+        //Задание 7
+       // Console.WriteLine("Введите предложение:");
+       // string st = Console.ReadLine();
+       //st = st.Trim();
+       // if (string.IsNullOrEmpty(st))
+       // {
+       //     Console.WriteLine("Предложение пусто.");
+       // }
+       // else
+       // {
+            
+       //     string[] words = st.Split(' ');
+
+           
+       //     int wordCount = words.Length;
+
+       //     Console.WriteLine("Количество слов в предложении: " + wordCount);
+       // }
+
+
+    }
+}
