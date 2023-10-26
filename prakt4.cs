3dcb558a5dea23324efd7b543f35ff84f91b161c
Оля 215 <122738326+olia215@users.noreply.github.com>
2023-10-26 10:52:46 +0300

    Add files via upload

---
+++ prakt4.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Задание 1. Вариант 2.удваивает каждое вхождение заданной подстроки 
+        //Console.WriteLine("Введите строку:");
+        //string s = Console.ReadLine();
+        //Console.WriteLine("Введите подстроку :");
+        //string x = Console.ReadLine();
+        //StringBuilder sb = new StringBuilder();
+        //Console.WriteLine(s.Replace(x, x + x));
+        //Console.ReadLine();
+        //Console.ReadKey();
+
+        //Задание 2.Дана строка, в которой содержится осмысленное текстовое сообщение. 
+        //Слова сообщения разделяются пробелами и знаками препинания.Вывести только те слова сообщения, которые содержат хотя бы одну цифру.
+        //Вариант 2
+        //Console.WriteLine("Введите строку:");
+        //string str = Console.ReadLine();
+        //string[] b = Regex.Split(str, @"\W+");
+        //var x = b.Where(word => Regex.IsMatch(word, @"\d"));
+        //foreach (string word in x)
+        //{
+        //    Console.WriteLine(word);
+        //}
+        //Console.ReadKey();
+    }
+}
