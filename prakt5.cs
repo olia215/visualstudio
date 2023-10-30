571711384e03faff31d424ec0dc021f38f65edd5
Оля 215 <122738326+olia215@users.noreply.github.com>
2023-10-30 10:29:13 +0200

    Add files via upload

---
+++ prakt5.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //1.Нахождение максимальной величины из двух целых переменных a, b.
+        //Console.WriteLine("Введите значение переменной a:");
+        //int a = int.Parse(Console.ReadLine());
+        //Console.WriteLine("Введите значение переменной b:");
+        //int b = int.Parse(Console.ReadLine());
+        //int max = a;
+        //if (b > a)
+        //{
+        //    max = b;
+        //}
+        //Console.WriteLine("Максимальное значение из a и b: " + max);
+
+        //2.Вычислить среднее арифметическое трех действительных чисел.
+        //Console.Write("Введите 1-е число:");
+        //double n = double.Parse(Console.ReadLine());
+        //Console.Write("Введите 2-е число:");
+        //double x = double.Parse(Console.ReadLine());
+        //Console.Write("Введите 3-е число:");
+        //double k = double.Parse(Console.ReadLine());
+        //double b = (n+x+k)/3;
+        //Console.WriteLine("Среднее арифметическое 3-х чисел:" + b);
+
+        //3 Составьте таблицу значений функции y = 4x² +5x - 10 на отрезке[-9; 9] с шагом h = 3.
+        //int a = -9; 
+        //int b = 9; 
+        //int h = 3; 
+        //Console.WriteLine("x\t y");
+        //for (int x = a; x <= b; x += h)
+        //{
+        //    int y = 4 * x * x + 5 * x - 10;
+        //    Console.WriteLine($"{x}\t {y}");
+        //}
+
+
+
+
+
+
+
+    }
+}
