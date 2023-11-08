737e5e63961d003b46c6c99d945e3029467e53f9
Оля 215 <122738326+olia215@users.noreply.github.com>
2023-11-08 21:46:44 +0200

    Add files via upload

---
+++ prakt5.1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+
+        //Вариант 8(Уровень B) 1. Рассчитать значение y, определив и использовав статический метод: 
+        //static double Func()
+        // { double x;
+        //     x = (1 + Math.Sin(4)) / (4 + Math.Sin(1)) + (7 + Math.Sin(5)) / (5 + Math.Sin(7)) + (3 + Math.Sin(2)) / (2 + Math.Sin(3));
+        //     return(x);
+
+        // }
+        // static void Main()
+        // {
+
+        //     Console.WriteLine(Func());
+        // }
+
+        //2. Определить статический метод void Sum(int n), который выводит на экран сумму цифр трехзначного числа n.Используя этот метод, вывести на экран сумму цифр чисел:
+        //а) 131; б) 547; в) m(m вводится с клавиатуры).
+        //static void Sum (int n)
+        //{
+        //    int a, b, c;
+        //    a = n / 100;
+        //    b = n / 10 - a*10;
+        //    c = n % 10;
+        //    int sum;
+        //    sum = a + b + c;
+        //    Console.WriteLine(sum);
+        //}
+        //static void Main()
+        //{
+        //    Sum(131);
+        //    Sum(547);
+        //    Console.Write("Напишите число m=");
+        //    int m =int.Parse(Console.ReadLine());
+        //    Sum(m);
+        //}
+
+        //3. Определить статический метод int Count(int n), который возвращает количество делителей натурального числа n.Используя этот метод, найти количество делителей для чисел:
+        //а) 19;б) 100 ; в) m(m вводятся с клавиатуры).
+        //static int Count (int n)
+        //{
+        //    int d=0;
+        //    for(int i=1; i<=n; i++)
+        //    {
+        //       if (n % i==0)
+        //        {
+        //            d = d+1;
+        //        }
+        //    }
+        //    return (d);
+        //}
+        //static void Main()
+        //{
+
+        //    Console.WriteLine(Count(19));
+        //    Console.WriteLine(Count(100));
+        //    Console.Write("Напишите число m=");
+        //    int m =int.Parse(Console.ReadLine());
+        //    Console.WriteLine(Count(m));
+
+
+
+        }
+
+
+
+
+
+    }
+}
