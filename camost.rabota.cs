2857ffc5085369045b44a6b20394cd6a100f2bcf
Оля 215 <122738326+olia215@users.noreply.github.com>
2023-11-08 20:52:55 +0200

    Add files via upload

---
+++ camost.rabota.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        //14.Реализовать функцию возведения в квадрат суммы двух вещественных чисел
+     // static double Func(double x, double y)
+     // {
+     //  double sum;
+     //  sum=Math.Pow(x + y,2);
+     // return (sum);
+         
+     // }
+     //static void Main ()
+     //{      
+     //       Console.Write("Введите 1-е число:");
+     //       double x = double.Parse(Console.ReadLine());
+     //       Console.Write("Введите 2-е число:");
+     //       double y = double.Parse(Console.ReadLine());
+     //       Console.WriteLine(Func(x,y));
+
+
+     //   }
+
+        
+    }
+}
