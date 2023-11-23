beb0c13a45ce1f91da1cc1ebcc07615ccf55fefa
Оля 215 <122738326+olia215@users.noreply.github.com>
2023-11-23 11:08:45 +0200

    Add files via upload

---
+++ tema9prakt1.cs
@@ -0,0 +1,83 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //1. Создать пустой список целого типа.
+        //    List<int> ints = new List<int>();
+        //    //2. Добавить следующие элементы в коллекцию: 1, 10, 25, 17, 10.
+        //    ints.Add(1);
+        //    ints.Add(10);
+        //    ints.Add(25);
+        //    ints.Add(17);
+        //    ints.Add(10);
+        //    //3. Через дополнительный метод просмотреть содержимое списка.
+        //    Func(ints);
+        //    //4. Определить количество элементов списка
+        //    Console.WriteLine($"Count = {ints.Count}");
+        //    //5. Определить индекс элемента со значением, равным 25.
+        //    Console.WriteLine($"Индекс 25: = {ints.IndexOf(25)}");
+        //    //6.Удалить из списка первый элемент со значением 10 и через дополнительный метод просмотреть содержимое списка.
+        //    ints.RemoveAt(1);//Удаляет 10
+        //    Func(ints);
+        //    Console.WriteLine();
+        //    //7. Удалить из списка элемент с индексом, равным 2 и через дополнительный метод просмотреть содержимое списка.
+        //    ints.RemoveAt(2);//Удаляет 17
+        //    Func(ints);
+        //    //8. Проверить наличие элемента со значением 25 в списке.
+        //    bool x = ints.Contains(25);
+        //    Console.WriteLine($"Индекс 25 = {x}");
+        //    //9.Вставить число 500 в самом начале списка и через дополнительный метод просмотреть содержимое списка.
+        //    ints.Insert(0, 500);
+        //    Func(ints);
+        //    Console.WriteLine();
+        //    //10. Отсортировать элементы списка по возрастанию и через дополнительный метод просмотреть содержимое списка.
+        //    ints.Sort();
+        //    Func(ints);
+        //    Console.WriteLine();
+        //    //11. Изменить значение нулевого элемента списка на величину, равную сумме первого элемента и числа
+        //    ints.Insert(0, ints[1] + 3);
+        //    Func(ints);
+        //    Console.ReadKey();
+        //}
+
+        //static void Func(List<int> ints)
+        //{
+        //    foreach (var elem in ints)
+        //    {
+
+        //        Console.WriteLine(elem);
+        //    }
+        //}
+        //Задание 
+        //2. Удалить все нечетные элементы списка, заполненного случайным образом целыми числами.
+            //List<int> ints = new List<int>();
+            //Random random = new Random();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    int a = random.Next(1, 100);
+            //    ints.Add(a);
+            //}
+            //Console.WriteLine(" список чисел:");
+            //foreach (int number in ints)
+            //{
+            //    Console.Write(number + " ");
+            //}
+            //for (int i = ints.Count - 1; i >= 0; i--)
+            //{
+            //    if (ints[i] % 2 != 0)
+            //    {
+            //        ints.RemoveAt(i);
+            //    }
+            //}
+            //Console.WriteLine("\n\nСписок чисел после удаления нечетных элементов:");
+            //foreach (int number in ints)
+            //{
+            //    Console.Write(number + " ");
+            //}
+            //Console.ReadLine();
+
+           
+        
+    }
+}
+
