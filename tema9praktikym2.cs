5bd747d8e8d9abd4de670aa759ad693ef7400c9d
Оля 215 <122738326+olia215@users.noreply.github.com>
2023-11-30 23:43:34 +0200

    Add files via upload

---
+++ tema9praktikym2.cs
@@ -0,0 +1,66 @@
+﻿using System.Runtime.Serialization.Formatters;
+using System.Security.Cryptography;
+
+internal class Program
+{
+    //private static void Main(string[] args)
+    //{
+    //    List<string> list = new List<string>();
+    //    list.Add("Россия");
+    //    list.Add("Франция");
+    //    list.Add("Германия");
+    //    list.Add("Италия");
+    //    list.Add("Испания");
+    //    list.Add("Греция");
+    //    list.Add("Португалия");
+    //    list.Add("Турция");
+    //    Dol(list);
+    //    Console.Write("\n");
+    //    Bel(list); Cs(list);
+    //    Console.Write("\n");
+    //    Dol(list);
+    //    Console.Write("\n");
+    //    Func(list); Bel(list);
+    //    Console.Write("\n");
+    //    Func(list);
+
+
+    //    Console.WriteLine($"Count:={list.Count}");
+    //    Console.ReadKey();
+
+    //}
+    //static void Cs(List<string> list)
+    //{
+    //    foreach(string cs in list)
+    //    {
+    //        Console.WriteLine(cs);
+    //    }
+    //}
+    //static void Dol(List<string> list)
+    //{
+    //    for(int i = 0; i < list.Count; i++)
+    //    {
+    //        list[i]=list[i].ToUpper();
+    //    }
+    //    foreach(string dol in list)
+    //    {
+    //        Console.WriteLine(dol);
+    //    }
+    //}
+    //static void Bel(List<string> list)
+    //{
+    //    list.RemoveAt(3);
+    //    foreach(string bel in list)
+    //    {
+    //        Console.WriteLine(bel);
+    //    }
+    //}
+    //static void Func(List<string> list)
+    //{
+    //    list.Reverse();
+    //    foreach(string func in list)
+    //    {
+    //        Console.WriteLine(func);
+    //    }
+    //}
+}
