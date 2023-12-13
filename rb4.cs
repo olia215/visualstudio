e44b8aa5199535fad1a7955038e1d557c9b9f894
Оля 215 <122738326+olia215@users.noreply.github.com>
2023-12-14 00:29:09 +0200

    Add files via upload

---
+++ rb4.cs
@@ -0,0 +1,53 @@
+﻿using ConsoleApp25;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Задание 1.
+        //Figures figure1 = new Figures();
+        //figure1.name = "Прямоугольник";
+        //figure1.side1 = 5;
+        //figure1.side2 = 3;
+
+        //Figures figure2 = new Figures("Квадрат", 4, 4);
+
+        //Figures figure4 = new Figures();
+        //figure4.name = "Треугольник";
+        //figure4.side1 = 3;
+        //figure4.side2 = 4;
+
+        //Figures figure5 = new Figures("Круг", 5, 0);
+ 
+        //figure1.ShowInfo();
+        //Console.WriteLine("Периметр: " + figure1.Perimetr());
+        //Console.WriteLine("Площадь: " + figure1.Formyla());
+
+        //figure2.ShowInfo();
+        //Console.WriteLine("Периметр: " + figure2.Perimetr());
+        //Console.WriteLine("Площадь: " + figure2.Formyla());
+
+        //figure4.ShowInfo();
+        //Console.WriteLine("Периметр: " + figure4.Perimetr());
+        //Console.WriteLine("Площадь: " + figure4.Formyla());
+
+        //figure5.ShowInfo();
+        //Console.WriteLine("Периметр: " + figure5.Perimetr());
+        //Console.WriteLine("Площадь: " + figure5.Formyla());
+
+
+        //Задание 2. Вариант 5
+       
+        //Tovar notebook = new Tovar("Тетрадь", 150, true, 10);
+        //notebook.Product();
+
+        //Tovar book = new Tovar("Книга", 500, false, 5);
+        //book.Product();
+
+        //Tovar pen = new Tovar("Ручка", 50, true, 20);
+        //pen.Product();
+
+    }
+
+
+}
