d7744df6e7dea42bc82adc9244c8c414bd5d8451
olia115 <dacenkoola3@gmail.com>
2023-12-21 02:29:18 +0200

    add ConsoleApp29

---
+++ ConsoleApp29/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+    //internal class Person
+    //{
+    //    public string name;
+    //    public string surname;
+    //    public int age;
+    //    public Person() { }
+    //    public Person(string name)
+    //    { this.name = name; }
+
+    //    public Person(string name, string surname)
+    //    {
+    //        this.name = name;
+    //        this.surname = surname;
+    //    }
+    //    public Person(string name, string surname, int age)
+    //    {
+    //        this.name = name;
+    //        this.surname = surname;
+    //        this.age = age;
+    //    }
+    //    public void GetInfo()
+    //    {
+    //        Console.WriteLine($"name-{name}\n surname-{surname}\n age-{age}");
+    //    }
+    //}
+}
