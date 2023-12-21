73b51b838662a8381f15e2f55ac39fef670bd8b2
olia115 <122738326+olia115@users.noreply.github.com>
2023-12-21 02:22:49 +0200

    First Commit
    First Commit

---
+++ ConsoleApp29/Dog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+    //internal class Dog
+    //{
+    //    public string Breed { get; set; }
+    //    public string Size { get; set; }
+    //    public int Age { get; set; }
+    //    public string Color { get; set; }
+    //    public void Eat()
+    //    {
+    //        Console.WriteLine("Собака ест");
+    //    }
+
+    //    public void Sleep()
+    //    {
+    //        Console.WriteLine("Собака спит");
+    //    }
+
+    //    public void Sit()
+    //    {
+    //        Console.WriteLine("Собака сидит");
+    //    }
+
+    //    public void Run()
+    //    {
+    //        Console.WriteLine("Собака бежит");
+    //    }
+    //}
+}
