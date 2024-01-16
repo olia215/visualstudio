81e1f562480527218d97054ca776e6d8ecbfc9fa
olia215 <dacenkoola3@gmail.com>
2024-01-16 23:15:44 +0200

    First Commit
    First Commit

---
+++ ConsoleApp31/Point.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    internal class Point
+    {
+        ////Задача 2.
+        //public int x { get; set; }
+        //public int y { get; set; }
+
+        ///// <summary>
+        ///// конструктор
+        ///// </summary>
+        ///// <param name="x"></param>
+        ///// <param name="y"></param>
+        //public Point(int x, int y)
+        //{
+        //    x = x;
+        //    y = y;
+        //}
+
+        ///// <summary>
+        ///// статический метод для вычисления расстояния между двумя точками
+        ///// </summary>
+        ///// <param name="point1"></param>
+        ///// <param name="point2"></param>
+        ///// <returns></returns>
+        //public static double LengthPoints(Point point1, Point point2)
+        //{
+        //    int a = point2.x - point1.x;
+        //    int b = point2.y - point1.y;
+
+        //    return Math.Sqrt(a * a + b * b);
+        //}
+
+
+        //Задача 1
+        //private double x;
+        //private double y;
+        //private static int count;
+
+        //public Point(double x, double y)
+        //{
+        //    this.x = x;
+        //    this.y = y;
+        //    count++;
+        //}
+
+        //public Point()
+        //{
+        //    this.x = 1.0;
+        //    this.y = 1.0;
+        //    count++;
+        //}
+
+        //static Point()
+        //{
+        //    count = 0;
+        //}
+
+        //public double GetX()
+        //{
+        //    return this.x;
+        //}
+
+        //public double GetY()
+        //{
+        //    return this.y;
+        //}
+
+        //public void SetX(double x)
+        //{
+        //    this.x = x;
+        //}
+
+        //public void SetY(double y)
+        //{
+        //    this.y = y;
+        //}
+
+        //public static int GetCount()
+        //{
+        //    return count;
+        //}
+
+        //public void Print()
+        //{
+        //    Console.WriteLine("координаты точки: ({0}, {1})", this.x, this.y);
+        //}
+        //public static int GetInstanceCount()
+        //{
+        //    return count;
+        //}
+    }
+}
