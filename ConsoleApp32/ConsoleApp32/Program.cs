1fb48afbc64ab5af9268088052c62b6e708c1d07
olia215 <dacenkoola3@gmail.com>
2024-01-18 23:33:56 +0200

    First Commit
    First Commit

---
+++ ConsoleApp32/ConsoleApp32/Program.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleApp32;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        MyClass myClass1 = new MyClass();
+
+        Console.WriteLine("количества созданных экземпляров: " + MyClass.count);
+
+
+        myClass1.Print();
+        Console.ReadKey();
+
+    }
+}
