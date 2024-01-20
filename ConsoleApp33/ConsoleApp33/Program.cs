b1407390576a340c68e68c9b7724a01b6d575c86
olia215 <dacenkoola3@gmail.com>
2024-01-20 16:54:24 +0200

    First Commit
    First Commit

---
+++ ConsoleApp33/ConsoleApp33/Program.cs
@@ -0,0 +1,68 @@
+﻿using ConsoleApp33;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //object[] arr = new object[6]; 
+        //arr[0] = 1859; //arr[1] = -7; 
+        //arr[2] = 'g'; //arr[3] = "hello"; 
+        //arr[4] = null; //arr[5] = new object(); 
+        //foreach (var point in arr) 
+        //{ // Console.WriteLine($"{arr}"); 
+        //} //Console.ReadKey(); 
+
+
+        /// <summary>
+        /// создаем массив
+        /// </summary> 
+
+        Students[] students = new Students[3];
+        /// <summary>
+        /// Ввод информации о школьниках
+        /// </summary> 
+
+        for (int i = 0; i < students.Length; i++)
+        {
+            Console.WriteLine($"Информация о студентах: {i + 1}:");
+            Console.Write("Фамилия: "); string fullName = Console.ReadLine();
+            Console.Write("пол: "); string gender = Console.ReadLine();
+            Console.Write("год рождения: "); int birthYear = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            students[i] = new Students(fullName, gender, birthYear);
+        }
+
+        int x = 0; int y = 0;
+        /// <summary>
+        /// Подсчет количества мальчиков и девочек 
+        /// </summary> 
+
+        foreach (var student in students)
+        {
+            if (student.Gender.ToLower() == "мужской")
+            { x++; }
+            else if (student.Gender.ToLower() == "женский")
+            { y++; }
+        }
+        /// <summary>
+        /// вывод каждого из списка
+        /// </summary>
+        Console.WriteLine($"количество мальчиков: {x}");
+        Console.WriteLine($"количество девочек: {y}");
+        Console.WriteLine("\nсписок мальчиков:");
+        foreach (var student in students)
+        {
+            if (student.Gender.ToLower() == "мужчина")
+            { Console.WriteLine(student.FullName); }
+        }
+        Console.WriteLine("\nсписок девочек:");
+        foreach (var student in students)
+        {
+            if (student.Gender.ToLower() == "женщина")
+            { Console.WriteLine(student.FullName); }
+        }
+        Console.ReadKey();
+    }
+} 
+
+    
---
+++ ConsoleApp33/ConsoleApp33/Students.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp33
+{
+    //internal class Students
+    //{
+    //    public string FullName;
+
+    //    public string Gender;
+
+    //    public int BirthYear;
+
+
+
+    //    public Students(string fullName, string gender, int birthYear)
+
+    //    {
+
+    //        FullName = fullName;
+
+    //        Gender = gender;
+
+    //        BirthYear = birthYear;
+    //    }
+    //}
+}
