f8d0c098539f71c2e4a64d19e0e6d342000486f9
olia215 <dacenkoola3@gmail.com>
2024-02-01 23:20:12 +0200

    First Commit
    First Commit

---
+++ ConsoleApp36/ConsoleApp36/DateClass.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp36
+{
+    internal class DateClass
+    {/// <summary>
+     /// поле DateTime data
+     /// </summary>
+        private DateTime data;
+        /// <summary>
+        /// конструктор без параметров
+        /// </summary>
+        public DateClass()
+        {
+            data = DateTime.Now;
+        }
+        /// <summary>
+        ///  конструктор, позволяющий установить :заданную дату(конструктор с тремя параметрами – год, месяц, день)
+        /// </summary>
+        /// <param name="year"></param>
+        /// <param name="month"></param>
+        /// <param name="day"></param>
+        public DateClass(int year, int month, int day)
+        {
+            data = new DateTime(year, month, day);
+        }
+        /// <summary>
+        /// свойство, позволяющее установить или получить значение поле класса(доступно для чтения и записи)
+        /// </summary>
+        public DateTime Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+        /// <summary>
+        ///  методы, позволяющие: вычислить дату предыдущего дня; вычислить дату следующего дня; определить сколько дней осталось до конца месяца.
+        /// </summary>
+        /// <returns></returns>
+        public DateTime GetPreviousDay()
+        {
+            return data.AddDays(-1);
+        }
+
+        public DateTime GetNextDay()
+        {
+            return data.AddDays(1);
+        }
+
+        public int GetDaysUntilEndOfMonth()
+        {
+            DateTime endOfMonth = new DateTime(data.Year, data.Month, DateTime.DaysInMonth(data.Year, data.Month));
+            return (endOfMonth - data).Days;
+        }
+
+    }
+}
---
+++ ConsoleApp36/ConsoleApp36/Program.cs
@@ -0,0 +1,59 @@
+﻿using ConsoleApp36;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine("Введите год, месяц и день:");
+
+        //string[] input = Console.ReadLine().Split(' ');
+        //int year = int.Parse(input[0]);
+        //int month = int.Parse(input[1]);
+        //int day = int.Parse(input[2]);
+
+        //DateClass date = new DateClass();
+
+        //Console.WriteLine("Предыдущий день: " + date.GetPreviousDay());
+        //Console.WriteLine("Следующий день: " + date.GetNextDay());
+        //Console.WriteLine("Дней до конца месяца: " + date.GetDaysUntilEndOfMonth());
+
+        ////Задание 2
+        //Console.WriteLine("Введите первую дату (год, месяц, день):");
+
+        //input = Console.ReadLine().Split(' ');
+        //DateTime date1 = new DateTime(int.Parse(input[0]), int.Parse(input[1]), int.Parse(input[2]));
+
+        //Console.WriteLine("Введите вторую дату (год, месяц, день):");
+        //input = Console.ReadLine().Split(' ');
+        //DateTime sd = new DateTime(int.Parse(input[0]), int.Parse(input[1]), int.Parse(input[2]));
+
+        //TimeSpan difference = sd - date1;
+        //Console.WriteLine("Количество дней между датами: " + difference.Days);
+
+        ////Задание 3
+        //Console.WriteLine("Введите время начала первой процедуры (часы и минуты ):");
+        //input = Console.ReadLine().Split(' ');
+        //int sh = int.Parse(input[0]);
+        //int sm = int.Parse(input[1]);
+
+        //Console.WriteLine("Введите время начала следующей процедуры (часы и минуты):");
+        //input = Console.ReadLine().Split(' ');
+        //int nextStartHour = int.Parse(input[0]);
+        //int nextStartMinute = int.Parse(input[1]);
+
+        //Console.WriteLine("Введите общее количество назначенных процедур:");
+        //int totalCount = int.Parse(Console.ReadLine());
+
+        //TimeSpan timeBetweenProcedures = new TimeSpan(0, nextStartHour - sh, nextStartMinute - sm);
+
+        //DateTime currentTime = new DateTime(2024, 02, 01, sh, sm, 0);
+        //Console.WriteLine("Время проведения процедур:");
+        //for (int i = 0; i < totalCount; i++)
+        //{
+        //    Console.WriteLine(currentTime);
+        //    currentTime += timeBetweenProcedures;
+        //}
+        //Console.ReadKey();
+
+    }
+}
