9f0066505f5226ac94b622c9fa0605f878cdb105
Оля 215 <122738326+olia215@users.noreply.github.com>
2024-03-05 00:24:41 +0200

    Update Teacher.cs

--- ConsoleApp45/ConsoleApp45/Teacher.cs
+++ ConsoleApp45/ConsoleApp45/Teacher.cs
@@ -8,8 +8,20 @@ namespace ConsoleApp45
 {
     internal class Teacher : Person
     {
-        public string position;
-        public int experience;
+        protected string position;
+        protected int experience;
+
+        public string Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public int Experience
+        {
+            get { return experience; }
+            set { experience = value; }
+        }
 
         public Teacher(string name, string lastname, DateTime birth, string faculty, string position, int experience) : base(name, lastname, birth, faculty)
         {
@@ -19,7 +31,24 @@ namespace ConsoleApp45
 
         public override void Info()
         {
-            Console.WriteLine($"Учитель: {name}, {lastname}, Возраст: {Age()}, Факультет:{faculty}, Должность: {position}, Стаж: {experience}");
+            Console.WriteLine($"Teacher: {name}, {lastname}, Age: {Age()}, Faculty:{faculty}, Position: {position}, Experience: {experience}");
+        }
+
+        public static Teacher Input()
+        {
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+            Console.WriteLine("введите фамилию");
+            string lastname = Console.ReadLine();
+            Console.WriteLine("Введите возраст");
+            DateTime birth = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Введите факультет");
+            string faculty = Console.ReadLine();
+            Console.WriteLine("Введите должность");
+            string position = Console.ReadLine();
+            Console.WriteLine("введите стаж");
+            int experience = Convert.ToInt32(Console.ReadLine());
+            return new Teacher(name, lastname, birth, faculty, position, experience);
         }
     }
 }
