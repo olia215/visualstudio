0013fc96bd53e1fd20728f2c8a972354edb4860c
Оля 215 <122738326+olia215@users.noreply.github.com>
2024-03-05 00:24:15 +0200

    Update Student.cs

--- ConsoleApp45/ConsoleApp45/Student.cs
+++ ConsoleApp45/ConsoleApp45/Student.cs
@@ -8,8 +8,13 @@ namespace ConsoleApp45
 {
     internal class Student : Person
     {
-        public int course;
+         protected int course;
 
+        public int Course
+        {
+            get { return course; }
+            set { course = value; }
+        }
         public Student(string name, string lastname, DateTime birth, string faculty, int course) : base(name, lastname, birth, faculty)
         {
             this.course = course;
@@ -17,7 +22,22 @@ namespace ConsoleApp45
 
         public override void Info()
         {
-            Console.WriteLine($"Студент: {name}, {lastname}, Возраст: {Age()}, Факультет:{faculty}, Курс: {course}");
+            Console.WriteLine($"Student: {name}, {lastname}, Age: {Age()}, Faculty:{faculty}, Course: {course}");
+        }
+
+        public static Student Input()
+        {
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+            Console.WriteLine("введите фамилию");
+            string lastname = Console.ReadLine();
+            Console.WriteLine("Введите возраст");
+            DateTime birth = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Введите факультет");
+            string faculty = Console.ReadLine();
+            Console.WriteLine("Введите курс");
+            int course = Convert.ToInt32(Console.ReadLine());
+            return new Student(name, lastname, birth, faculty, course);
         }
     }
 }
