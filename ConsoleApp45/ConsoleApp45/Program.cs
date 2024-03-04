d9ffb91e8a9f591b9e8459f3de672d67fc97a6f8
Оля 215 <122738326+olia215@users.noreply.github.com>
2024-03-05 00:23:48 +0200

    Update Program.cs

--- ConsoleApp45/ConsoleApp45/Program.cs
+++ ConsoleApp45/ConsoleApp45/Program.cs
@@ -7,19 +7,51 @@ internal class Program
         List<Person> people = new List<Person>();
 
 
-        people.Add(new Applicant("John", "Doe", new DateTime(2000, 7, 15), "Инженерный"));
-        people.Add(new Student("Alice", "Smith", new DateTime(1998, 3, 20), "Физический", 3));
-        people.Add(new Teacher("Bob", "Johnson", new DateTime(1985, 9, 10), "Математический", "Профессор", 10));
+        people.Add(new Applicant("John", "Doe", new DateTime(2000, 7, 15), "Engineering"));
+        people.Add(new Student("Alice", "Smith", new DateTime(1998, 3, 20), "Physics", 3));
+        people.Add(new Teacher("Bob", "Johnson", new DateTime(1985, 9, 10), "Mathematics", "Professor", 10));
 
         foreach (Person person in people)
         {
             person.Info();
         }
 
-        int minAge = 20;
-        int maxAge = 30;
+        bool flag = true;
+        while (flag)
+        {
+            Console.WriteLine("Введите название");
+            string s = Console.ReadLine();
+            switch (s)
+            {
+                case "Абитуриент":
+                    people.Add(Applicant.Input());
+                    break;
+                case "Студент":
+                    people.Add(Student.Input());
+                    break;
+                case "Учитель":
+                    people.Add(Teacher.Input());
+                    break;
+            }
 
-        Console.WriteLine($"\nЛюди в возрасте от {minAge} и до {maxAge} лет:");
+            foreach (var elem in people)
+            {
+                elem.Info();
+            }
+
+        }
+
+
+        Console.Write("Введите нижнюю границу");
+        int minAge = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите верхнюю границу");
+        int maxAge = Convert.ToInt32(Console.ReadLine()); ;
+
+        Console.WriteLine($"\nPeople aged between {minAge} and {maxAge} years:");
+        for (int i = 0; i < list.Count; i++)
+        {
+            if (list[i].Age() > minAge && list[i].Age() < maxAge) ;
+        }
         foreach (Person person in people)
         {
             if (person.Age() >= minAge && person.Age() <= maxAge)
