d4f4a649af8564bd789aad5de6fdc59344c303b5
Оля 215 <122738326+olia215@users.noreply.github.com>
2024-03-05 23:18:39 +0200

    Update Applicant.cs

--- ConsoleApp45/ConsoleApp45/Applicant.cs
+++ ConsoleApp45/ConsoleApp45/Applicant.cs
@@ -9,14 +9,29 @@ namespace ConsoleApp45
 {
     internal class Applicant : Person
     {
-        public Applicant(string name, string lastname, DateTime birth, string faculty) : base(name, lastname, birth, faculty)
+         public Applicant(string name, string lastname, DateTime birth, string faculty) : base(name, lastname, birth, faculty)
         {
-
+           
         }
 
         public override void Info()
         {
-            Console.WriteLine($"ФИО: {name}, {lastname}, Возраст: {Age()}, Факультет:{faculty}");
+            Console.WriteLine($"Applicant: {Name} {Lastname}");
+            Console.WriteLine($"Birth: {Birth.ToShortDateString()}");
+            Console.WriteLine($"Faculty:{Faculty}");
+            Console.WriteLine($"Age: {Age()}");
+        }
+        public static Applicant Input()
+        {
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+            Console.Write("введите фамилию: ");
+            string lastname = Console.ReadLine();
+            Console.Write("Введите возраст: ");
+            DateTime birth = DateTime.Parse(Console.ReadLine());
+            Console.Write("Введите факультет: ");
+            string faculty = Console.ReadLine();
+            return new Applicant(name, lastname, birth, faculty);
         }
 
     }
